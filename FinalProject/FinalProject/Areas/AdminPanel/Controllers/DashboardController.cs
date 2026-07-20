927be67a189ef25c213a870a7c9d64bee86eafdb
ilkin200500 <ikoyamda1@gmail.com>
2026-07-20 21:41:29 +0400

    20iyul

--- FinalProject/FinalProject/Areas/AdminPanel/Controllers/DashboardController.cs
+++ FinalProject/FinalProject/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FinalProject.DAL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace FinalProject.Areas.AdminPanel.Controllers
 {
-
-    //[Authorize(Roles ="Admin")]
+    // [Authorize(Roles ="Admin")] // Hər şey tam hazır olduqdan sonra bu sətri aktivləşdirə bilərsən
     [Area("AdminPanel")]
-    // [Route("Admin")] <-- Əgər belə bir sətir varsa, bunu mütləq sil və ya şərhə al!
     public class DashboardController : Controller
     {
-        // Bura açıq şəkildə View-nun tam yolunu yazaraq sistemi məcbur edirik:
-        public IActionResult Index()
+        private readonly CourseDbContext _context;
+
+        // Verilənlər bazası kontekstini (Context) buraya daxil edirik
+        public DashboardController(CourseDbContext context)
         {
+            _context = context;
+        }
+
+        // Sayları asinxron olaraq çəkmək üçün metodu "public async Task<IActionResult>" etdik
+        public async Task<IActionResult> Index()
+        {
+            // Bazadakı ümumi sayları dinamik olaraq hesablayıb ViewBag-ə doldururuq
+            ViewBag.TotalStudents = await _context.students.CountAsync();
+            ViewBag.TotalTeachers = await _context.teachers.CountAsync();
+            ViewBag.TotalGroups = await _context.Groups.CountAsync();
+            ViewBag.TotalCourses = await _context.courses.CountAsync();
+
             return View();
         }
     }
--- FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcb64cd5d9011ac08261d0413590eac8212b1e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5c9bd0bca54ad1fa23d9572e245c2a5ca428c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
